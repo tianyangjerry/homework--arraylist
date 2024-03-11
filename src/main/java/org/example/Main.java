aa03427d9cf31da576638e0e0e87112a1b433973
TianYangChen <159708159+tianyangjerry@users.noreply.github.com>
2024-03-11 20:21:37 +0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please stores the ID of the event");
+        String eventID = scanner.next();
+        System.out.println("Please stores the name of the event");
+        String eventName = scanner.next();
+        System.out.println("Please stores the venue of the event");
+        String eventVenue = scanner.next();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        String eventDate = dtf.format(java.time.LocalDate.now());
+        Event event = new Event(eventID, eventName, eventVenue, eventDate);
+        event.organizeEvent();
+        System.out.println("Displaying the eventID of the event : " + event.getEventID());
+        System.out.println("Displaying the eventName of the event : " + event.getEventName());
+        System.out.println("Displaying the eventVenue of the event : " + event.getEventVenue());
+        System.out.println("Displaying the eventDate of the event : " + event.getEventDate());
+        System.out.println("Displaying the eventAttendees of the event");
     }
 }
